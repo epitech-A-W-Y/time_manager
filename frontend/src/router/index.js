f9fa8d408a322c3df1cb1d4312b21fdad0685793
Alaa Maachouk <alaamaachouk@gmail.com>
2026-09-23 13:42:52 +0000

    Add working time route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import User from '../components/User.vue'
 import WorkingTimes from '../components/WorkingTimes.vue'
+import WorkingTime from '../components/WorkingTime.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +17,11 @@ const router = createRouter({
       name: 'WorkingTimes',
       component: WorkingTimes,
       props: true
+    },
+    {
+      path: '/workingTime',
+      name: 'WorkingTime',
+      component: WorkingTime
     }
   ]
 })
